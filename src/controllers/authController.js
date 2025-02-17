4cac395bd34e4851135d01c46f1df84e95eb5d14
MD Rashid Alam <alamr9253@gmail.com>
2025-02-17 20:09:06 +0530

    Add changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -27,7 +27,7 @@ async function login(req,res) {
 
         res.cookie('authToken',response.token,{
             httpOnly:true,
-            secure:true,
+            secure:process.env.NODE_ENV === 'production',
             maxAge:7*24*60*60*1000
         })
 
