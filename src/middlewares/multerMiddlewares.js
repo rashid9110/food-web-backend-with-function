86052d456c64c14af33b5bbb0c6bc2b3565365be
MD Rashid Alam <alamr9253@gmail.com>
2025-04-30 21:43:58 +0530

    change multer file

--- src/middlewares/multerMiddlewares.js
+++ src/middlewares/multerMiddlewares.js
@@ -1,16 +1,34 @@
-const multer=require('multer');
+// const multer=require('multer');
 
-const path=require('path');
+// const path=require('path');
 
-const storageConfiguration=multer.diskStorage({
-    destination:(req,file,next)=>{
-        next(null,'uploads/');
-    },
-    filename:(req,file,next)=>{
-        console.log(file);
-        next(null,`${Date.now()}${path.extname(file.originalname)}`);
-    }
-})
+// const storageConfiguration=multer.diskStorage({
+//     destination:(req,file,next)=>{
+//         next(null,'uploads/');
+//     },
+//     filename:(req,file,next)=>{
+//         console.log(file);
+//         next(null,`${Date.now()}${path.extname(file.originalname)}`);
+//     }
+// })
 
-const uploader=multer({storage:storageConfiguration});
-module.exports=uploader;
+// const uploader=multer({storage:storageConfiguration});
+// module.exports=uploader;
+
+const multer = require('multer');
+const path = require('path');
+
+// Use /tmp for safe writing in Render
+const storageConfiguration = multer.diskStorage({
+  destination: (req, file, next) => {
+    next(null, '/tmp'); // ✅ render-compatible
+  },
+  filename: (req, file, next) => {
+    console.log('Uploading file:', file.originalname);
+    next(null, `${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
+
+const uploader = multer({ storage: storageConfiguration });
+
+module.exports = uploader;
